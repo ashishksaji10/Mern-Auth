af015a33ffeb1b52a292ad2684c0fe7809566605
ashishksaji10 <155794189+ashishksaji10@users.noreply.github.com>
2025-07-17 06:09:22 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
@@ -16,4 +17,5 @@ app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
 });
 
-export default app;
+
+app.use("/api/user", userRoute);
