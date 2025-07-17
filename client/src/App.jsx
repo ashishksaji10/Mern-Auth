0e15884ba6f04096ed0084fd1817a96f5847ba6a
ashishksaji10 <155794189+ashishksaji10@users.noreply.github.com>
2025-07-18 00:10:11 +0530

    complete sign up page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,8 +13,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
